b8ab15710047e86bfd6fc56ab5db2f7cf40ebe93
codeblue001 <JIDEDELANO@HOTMAIL.COM>
2019-03-12 00:21:59 -0400

    add core features

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+
+	private
+
+	def sign_up_params
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+	end
+
+	def acount_update_params
+		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+	end
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
