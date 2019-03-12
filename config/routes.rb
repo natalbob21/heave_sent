a69c2c3f12b3927962947c8eb39dbde8bbd9253a
codeblue001 <JIDEDELANO@HOTMAIL.COM>
2019-03-12 15:32:42 -0400

    create mutable timeline page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {resgistrations: 'resgistrations'}
   resources :events
   root "events#index"
   get "/:page" => "static#show"
