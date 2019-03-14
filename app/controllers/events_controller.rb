8f02c67c05c21df3907ec5bc1a06c8fc37c014a3
codeblue001 <JIDEDELANO@HOTMAIL.COM>
2019-03-14 11:12:43 -0400

    fix bugs with required form fields

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,7 +45,7 @@ before_action :find_event, only: [:show, :edit, :update, :destroy]
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :date, :message, :send_date)
+    params.require(:event).permit(:title, :description, :date, :message, :send_date, :phone, :recipient)
   end
 
   def find_event
