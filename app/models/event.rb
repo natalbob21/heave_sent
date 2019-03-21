0762da48614664805c39e96ade1a844189039589
codeblue001 <JIDEDELANO@HOTMAIL.COM>
2019-03-21 05:22:39 -0400

    create a new landing page

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,19 +8,6 @@ class Event < ApplicationRecord
 
 # Notify our appointment attendee X minutes before the appointment time
 
-<<<<<<< HEAD
-#
-# def send_event
-#   account_sid =  ENV['TWILIO_ACCOUNT_SID']
-#   auth_token = ENV['TWILIO_AUTH_TOKEN']
-#   @client = Twilio::REST::Client.new(account_sid, auth_token)
-#   @message = @client.messages.create(
-#    to: "+19546732891",
-#    from: ENV['TWILIO_NUMBER'],
-#    body: "Hello, it works!"
-# )
-# end
-=======
 
 def send_event
   account_sid =  ENV['TWILIO_ACCOUNT_SID']
@@ -32,8 +19,6 @@ def send_event
    body: "Hello, it works!"
 )
 end
->>>>>>> 0e95d79efae247a5f49be568df35aaa753c8082e
-
   # def when_to_run
   #   minutes_before_event = 30.minutes
   #   time - minutes_before_appointment
