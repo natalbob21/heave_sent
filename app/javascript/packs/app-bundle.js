cccdde1a5e1939da54d6d43594123dfa609d0ff7
Natalie Dinkins <natalie.dinkins1@gmail.com>
2019-03-21 18:16:01 -0400

    deleted react calendar

--- app/javascript/packs/app-bundle.js
+++ app/javascript/packs/app-bundle.js
@@ -1,8 +1,5 @@
 import ReactOnRails from "react-on-rails";
 
-import Calendar from "bundles/Calendar/components/Calendar";
-
 // This is how react_on_rails can see the Components in the browser.
 ReactOnRails.register({
-  Calendar
 });
